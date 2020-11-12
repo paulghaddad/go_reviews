2c1ffb5cbbc16d034a1119fa9d7ab91d1afe0f49
Paul Haddad <paulh16@gmail.com>
2020-11-12 14:15:24 -0500

    Select start

---
+++ select/select_test.go
@@ -0,0 +1,15 @@
+package select_go
+
+import "testing"
+
+func TestRacer(t *testing.T) {
+	slowURL := "http://www.facebook.com"
+	fastURL := "http://www.quii.co.uk"
+
+	want := fastURL
+	got := Racer(slowURL, fastURL)
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
